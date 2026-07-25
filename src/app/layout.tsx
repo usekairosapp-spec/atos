6832ce616d432a624ec34636a41486bd6ee403e8
usekairosapp-spec <242861969+usekairosapp-spec@users.noreply.github.com>
2026-07-25 03:00:07 -0300

    Verifica dominio no Google Search Console

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,9 @@ export const metadata: Metadata = {
   },
   description: "Agenda de Times, Organização e Serviço.",
   applicationName: "ATOS",
+  verification: {
+    google: "lCGIyVkbvIuiI5itTw4j-P4XxyD1JwVosJETE2YjOQo",
+  },
   manifest: "/manifest.webmanifest",
   appleWebApp: { capable: true, statusBarStyle: "default", title: "ATOS" },
   formatDetection: { telephone: false },
